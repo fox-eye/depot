ae34622c46a64da307430515f9ab8f01780b2e08
Lucas Romano <romano.lucas@gmail.com>
2010-12-15 00:11:11 +0100

    adding cart and testings

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,29 @@
 class Product < ActiveRecord::Base
-    validates :title, :description, :image_url, :price, :presence =>true
 
-    validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+     #add a default scope
+     default_scope :order => 'title'
 
-    validates :title, :uniqueness => true
+     has_many :line_items
 
-    validates :image_url, :format => {
-    :with => %r{(gif|jpg|png)$}i,
-    :message => "must be gif, jpg or png file"
-    }
+     before_destroy :ensure_not_referenced_by_any_line_item
+
+     def ensure_not_referenced_by_any_line_item
+	if(line_items.count.zero?)
+	     return true
+	else
+	     errors.add(:base,'Line Items present')
+	     return false
+	end
+     end
+
+     validates :title, :description, :image_url, :price, :presence =>true
+
+     validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+
+     validates :title, :uniqueness => true
+
+     validates :image_url, :format => {
+	:with => %r{(gif|jpg|png)$}i,
+	:message => "must be gif, jpg or png file"
+     }
 end
