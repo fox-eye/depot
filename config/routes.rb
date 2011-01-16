0c2d9fe430e578a4cfd343ddc392b1161c1e459d
Lucas Romano <romano.lucas@gmail.com>
2011-01-17 00:18:46 +0100

    add login / logout controllers, routes and views - needs limiting access

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Depot::Application.routes.draw do
+  get 'admin' => "admin#index"
+
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  resources :users
+
   resources :orders
 
   resources :line_items
 
   resources :carts
-
-  get "store/index"
 
   resources :products do
     get :who_bought, :on => :member
